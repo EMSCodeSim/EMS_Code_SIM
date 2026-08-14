241378086ae646bea04beef02c602f5b5a2d44bf
EMSCodeSim <davidagallaher@hotmail.com>
2026-08-14 09:51:47 -0600

    Refresh corrected communication grid

--- vitals/horse-crush-bootstrap.js
+++ vitals/horse-crush-bootstrap.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const CASE_ID = 'horse_crush';
-  const BUILD = '2026.08.14.10';
+  const BUILD = '2026.08.14.11';
 
   function loadOnce(attribute, src) {
     if (document.querySelector(`script[${attribute}]`)) return;
