72ff965b22a8aed96dff15e674007a0b129b315b
EMSCodeSim <davidagallaher@hotmail.com>
2026-08-14 10:50:21 -0600

    Release communication center improvements

--- vitals/horse-crush-bootstrap.js
+++ vitals/horse-crush-bootstrap.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const CASE_ID = 'horse_crush';
-  const BUILD = '2026.08.14.14';
+  const BUILD = '2026.08.14.15';
 
   function loadOnce(attribute, src) {
     if (document.querySelector(`script[${attribute}]`)) return;
