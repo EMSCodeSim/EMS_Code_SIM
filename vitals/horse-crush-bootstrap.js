5ca535121c0f0b07b0d35e9e76ecb3d33f780647
EMSCodeSim <davidagallaher@hotmail.com>
2026-08-14 12:05:49 -0600

    Release reliable ABC follow-up controls

--- vitals/horse-crush-bootstrap.js
+++ vitals/horse-crush-bootstrap.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const CASE_ID = 'horse_crush';
-  const BUILD = '2026.08.14.19';
+  const BUILD = '2026.08.14.20';
 
   function loadOnce(attribute, src) {
     if (document.querySelector(`script[${attribute}]`)) return;
