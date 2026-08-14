b628f6995d4d6990e8ee0ea299070705dc5f0312
EMSCodeSim <davidagallaher@hotmail.com>
2026-08-14 12:09:17 -0600

    Release direct Primary ABC interaction

--- vitals/horse-crush-bootstrap.js
+++ vitals/horse-crush-bootstrap.js
@@ -2,7 +2,7 @@
   'use strict';
 
   const CASE_ID = 'horse_crush';
-  const BUILD = '2026.08.14.20';
+  const BUILD = '2026.08.14.21';
 
   function loadOnce(attribute, src) {
     if (document.querySelector(`script[${attribute}]`)) return;
